e01a9d4fdb61b75da2f91053dbc38f76d40524f0
Vladimir Khalzov <vladimir.khalzov@gmail.com>
2020-05-20 01:56:35 +0300

    Updates

---
+++ precache-manifest.29f786aeccfe271972283734bfe60c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f2a2dbb55f3154de833403c3a3aab7",
+    "url": "/study-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "be1207f54450106f479f",
+    "url": "/study-monsters-rolodex/static/css/main.d1e3bc17.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/study-monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study-monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1207f54450106f479f",
+    "url": "/study-monsters-rolodex/static/js/main.e9d335e6.chunk.js"
+  },
+  {
+    "revision": "1e194d3d00c1075543e9",
+    "url": "/study-monsters-rolodex/static/js/runtime-main.2500a40f.js"
+  }
+]);
